e65f77b8819ab74f64477589b38e90eed4e6195b
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-07-06 18:16:16 +0700

    Add dialog to submit book

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
 const myLibrary = [];
 
-function Book(title, author, pages, readed) {
+function Book(title, author, pages, status) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.readed = readed;
+  this.status = status;
 }
 
 Book.prototype.info = function () {
   return (
     `${this.title} by ${this.author}, ${this.pages} pages, ` +
-    (this.readed ? "readed" : "not read yet")
+    (this.status ? "readed" : "not read yet")
   );
 };
 
-function addBookToLibrary(title, author, pages, readed) {
-  const newBook = new Book(title, author, pages, readed);
+function addBookToLibrary(title, author, pages, status) {
+  const newBook = new Book(title, author, pages, status);
 
   myLibrary.push(newBook);
 }
 
+const tablebody = document.getElementById("book-table-body");
+
 function displayBook() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    const element = myLibrary[i];
-    // console.log(
-    //   `ID:${element.id}|| ${element.info()}`
-    // );    
-  }
-} 
+  myLibrary.forEach((book) => {
+    const row = document.createElement("tr");
+
+    let readStatus = book.status ? "readed" : "unread";
+
+    const titleCell = document.createElement("td");
+    titleCell.classList.add("tg-0lax");
+    titleCell.textContent = book.title;
+    row.appendChild(titleCell);
+
+    const authorCell = document.createElement("td");
+    authorCell.classList.add("tg-0lax");
+    authorCell.textContent = book.author;
+    row.appendChild(authorCell);
+
+    const pagesCell = document.createElement("td");
+    pagesCell.classList.add("tg-0lax");
+    pagesCell.textContent = book.pages;
+    row.appendChild(pagesCell);
+
+    const statusCell = document.createElement("td");
+    statusCell.classList.add("tg-0lax");
+    statusCell.textContent = readStatus;
+    row.appendChild(statusCell);
+
+    tablebody.appendChild(row);
+  });
+}
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, true);
@@ -36,4 +59,21 @@ addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, false);
 addBookToLibrary("1984", "George Orwell", 328, true);
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
 
-displayBook()
+document.addEventListener("DOMContentLoaded", displayBook);
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+const submitBtn = document.querySelector("#submit-button");
+
+const output = document.querySelector("output");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+
+dialog.addEventListener("close", (e)=>{
+  output.value ="dqwvac"
+})
